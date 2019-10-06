62fc9e53763d120e764251c4c89e26f9bb4edbfd
gjy gjy <xusysh@outlook.com>
2019-10-06 20:16:02 +0800

    update

--- src/app/pages/admin/check-question/check-question.module.ts
+++ src/app/pages/admin/check-question/check-question.module.ts
@@ -4,11 +4,11 @@ import { CheckQuestionComponent } from './check-question.component';
 
 import { NgZorroAntdModule} from 'ng-zorro-antd';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  imports: [NgZorroAntdModule,CommonModule],
+  imports: [NgZorroAntdModule,CommonModule,FormsModule,ReactiveFormsModule],
   declarations: [CheckQuestionComponent],
   exports: [CheckQuestionComponent]
 })
