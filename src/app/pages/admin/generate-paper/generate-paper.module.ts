a09d5a8c4f8a69eac4609d8371bed831be69d47d
gjy gjy <xusysh@outlook.com>
2019-10-11 23:37:48 +0800

    update

--- src/app/pages/admin/generate-paper/generate-paper.module.ts
+++ src/app/pages/admin/generate-paper/generate-paper.module.ts
@@ -5,11 +5,10 @@ import { GeneratePaperComponent } from './generate-paper.component';
 import { NgZorroAntdModule} from 'ng-zorro-antd';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NzCodeBoxModule } from 'src/app/tools/nz-codebox/nz-codebox.module';
 
 
 @NgModule({
-  imports: [NgZorroAntdModule,CommonModule,FormsModule,ReactiveFormsModule,NzCodeBoxModule],
+  imports: [NgZorroAntdModule,CommonModule,FormsModule,ReactiveFormsModule],
   declarations: [GeneratePaperComponent],
   exports: [GeneratePaperComponent]
 })
