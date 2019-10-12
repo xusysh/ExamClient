defa7b6a59198926f0b4f025c2ab95e5dad362b1
gjy gjy <xusysh@126.com>
2019-10-12 15:59:34 +0800

    update 生成试卷

--- src/app/pages/admin/generate-paper/generate-paper.component.ts
+++ src/app/pages/admin/generate-paper/generate-paper.component.ts
@@ -14,7 +14,7 @@ import { forkJoin } from 'rxjs';
 export class GeneratePaperComponent implements OnInit {
 
   search_value = '';
-  selectedValue = null;
+  selected_value = null;
   nodes = [
     {
       title: '计算机专业核心',
@@ -87,6 +87,99 @@ export class GeneratePaperComponent implements OnInit {
       isLeaf: true
     }
   ];
+
+  selected_questions = [
+    {
+      'field_name': '数据结构',
+      'questions': [
+        {
+          'type': 'multi',
+          'answer': ['A', 'B'],
+          'content': '时间复杂度为O(nlog2n)的排序算法有（          ）',
+          'description': '',
+          'options': ['A.快速排序', 'B.堆排序', 'C.冒泡排序', 'D.折半插入排序']
+        },
+        {
+          'type': 'multi',
+          'answer': ['B', 'C'],
+          'content': '若入栈序列为A B C D E F，且进栈和出栈可以穿插进行，则不可能的输出序列为',
+          'description': '',
+          'options': ['']
+        },
+        {
+          'type': 'single',
+          'answer': ['B'],
+          'content': '若某表最常用的操作是在最后一个结点之后插入一个节点或删除最后一二个结点，则采用（）省运算时间。',
+          'description': '',
+          'options': ['']
+        },
+        {
+          'type': 'multi',
+          'answer': ['A', 'B'],
+          'content': '时间复杂度为O(nlog2n)的排序算法有（          ）',
+          'description': '',
+          'options': ['A.快速排序', 'B.堆排序', 'C.冒泡排序', 'D.折半插入排序']
+        },
+        {
+          'type': 'single',
+          'answer': ['C'],
+          'content': '对于int *pa[5] ;的描述，正确的是（    ）',
+          'description': '',
+          'options': [
+            "A.pa是一个指向数组的指针，所指向的数组是5个int型元素",
+            "B.pa是一个指向某个数组第5个元素的指针，该元素是int型变量",
+            "C.pa[5]表示某个数组第5个元素的值",
+            "D.pa是一个具有5个元素的指针数组，每个元素是一个int型指针"
+          ]
+        }
+      ]
+    },
+    {
+      'field_name': '计算机组成原理',
+      'questions': [
+        {
+          'type': 'single',
+          'answer': ['A'],
+          'content': '下列关于RISC的叙述中，错误的是（）。',
+          'description': '',
+          'options': [
+            'A.RISC普遍采用微程序控制器',
+            'B.RISC大多数指令在一个时钟周期内完成',
+            'C.RISC的内部通用寄存器数量相对CISC多',
+            'D.RISC的指令数、寻址方式和指令格式种类相对CISC少'
+          ]
+        },
+        {
+          'type': 'single',
+          'answer': ['A'],
+          'content': '指出下列代码的缺陷（      ）。',
+          'description': 'float   f[10];\n\
+  // 假设这里有对f进行初始化的代码\n\
+  ….\n\
+  //for循环需要遍历f中所有元素\n\
+  for(int i = 0; i < 10;)\n\
+  {\n\
+  if( f[++i] == 0 )\n\
+  break;\n\
+  }',
+          'options': [
+            'A.for(int i = 0; i < 10;)这一行写错了',
+            'B.f是float型数据直接做相等判断有风险',
+            'C.f[++i]应该是f[i++]',
+            'D.没有缺陷']
+        }
+      ]
+    },
+    {
+      'field_name': '操作系统'
+    },
+    {
+      'field_name': '计算机网络'
+    },
+    {
+      'field_name': '数据库原理'
+    }
+  ]
 
   questions = [
     {
