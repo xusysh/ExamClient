dfce7345a0ee68f6949ce32091715bea2a980929
gjy gjy <xusysh@126.com>
2019-10-16 22:06:56 +0800

    update generate-paper

--- src/app/pages/admin/judge-paper/judge-paper.module.ts
+++ src/app/pages/admin/judge-paper/judge-paper.module.ts
@@ -2,18 +2,19 @@ import { NgModule } from '@angular/core';
 
 import { JudgePaperComponent } from './judge-paper.component';
 
-import { NgZorroAntdModule} from 'ng-zorro-antd';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  imports: [NgZorroAntdModule,CommonModule],
+  imports: [NgZorroAntdModule, CommonModule, FormsModule, ReactiveFormsModule],
   declarations: [JudgePaperComponent],
   exports: [JudgePaperComponent]
 })
 export class JudgePaperModule {
-  constructor(){
-    
+  constructor() {
+
   }
- }
+}
