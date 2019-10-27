4af8f5e94e9a1c7b7dcc922e06c793c2c76aab60
gjy gjy <xusysh@126.com>
2019-10-27 18:57:45 +0800

    update

--- src/app/pages/admin/check-paper/check-paper.module.ts
+++ src/app/pages/admin/check-paper/check-paper.module.ts
@@ -4,11 +4,12 @@ import { CheckPaperComponent } from './check-paper.component';
 
 import { NgZorroAntdModule} from 'ng-zorro-antd';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  imports: [NgZorroAntdModule,CommonModule],
+  imports: [NgZorroAntdModule,CommonModule,FormsModule,ReactiveFormsModule],
   declarations: [CheckPaperComponent],
   exports: [CheckPaperComponent]
 })
