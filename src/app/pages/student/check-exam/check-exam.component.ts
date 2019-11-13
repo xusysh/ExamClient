7ed295ecf8eff48a419ec1b7a340a16819412264
gjy gjy <xusysh@126.com>
2019-11-13 16:18:11 +0800

    update

--- src/app/pages/student/check-exam/check-exam.component.ts
+++ src/app/pages/student/check-exam/check-exam.component.ts
@@ -49,8 +49,7 @@ export class CheckExamComponent implements OnInit {
       response => {
         this.student_exam_info_list = response.data;
         this.student_exam_info_list_backup = response.data;
-        this.UpdateFilteredAndSortedData();
-        this.ResetSearch();
+        this.ResetArrayData();
         this.loading = false;
         this.message.create('success', '考试信息获取成功');
       },
@@ -58,10 +57,6 @@ export class CheckExamComponent implements OnInit {
         this.message.create('error', '考试信息获取失败：连接服务器失败');
         this.loading = false;
       });
-  }
-
-  UpdateFilteredAndSortedData() {
-    this.student_exam_info_list = this.filter_sort_service.GetFilteredArray(this.student_exam_info_list_backup,this.status_selected_filter_val,'status');
   }
 
   ParseDurationNum(duration:number):ShowTime {
@@ -85,18 +80,40 @@ export class CheckExamComponent implements OnInit {
     this.UpdateTableData();
   }
 
+  ResetArrayData() {
+    this.status_filter = [
+      { text: '未开始', value: '未开始' }, 
+      { text: '进行中', value: '进行中' },
+      { text: '判卷中', value: '判卷中' },
+      { text: '已结束', value: '已结束' }
+    ];
+    this.ResetSearch();
+  }
+
   StatusFilterChange(filter) {
     this.status_selected_filter_val = filter;
-    this.student_exam_info_list = this.filter_sort_service.GetFilteredArray(this.student_exam_info_list_backup,filter,'status');
+    this.UpdateFilteredData();
+  }
+
+  UpdateFilteredData() {
+    if(this.status_selected_filter_val.length == 0) {
+      this.ResetArrayData();
+      return;
+    }
+    this.student_exam_info_list = this.filter_sort_service.GetFilteredArray(this.student_exam_info_list,this.status_selected_filter_val,'status');
   }
 
   SearchNameInArray() {
-    this.student_exam_info_list = this.filter_sort_service.GetSearchedArray(this.student_exam_info_list_backup,this.search_exam_name_value,'examName');
+    if(this.search_exam_name_value == null||this.search_exam_name_value == "") {
+      this.ResetArrayData();
+      return;
+    }
+    this.student_exam_info_list = this.filter_sort_service.GetSearchedArray(this.student_exam_info_list,this.search_exam_name_value,'examName');
   }
 
   ResetSearch() {
     this.search_exam_name_value = "";
-    this.SearchNameInArray();
+    this.student_exam_info_list = this.student_exam_info_list_backup;
   }
 
 }
--- src/app/tools/FilterSortService.component.ts
+++ src/app/tools/FilterSortService.component.ts
@@ -8,7 +8,6 @@ export class FilterSortService {
 	constructor() { }
 
 	GetFilteredArray(arr: Array<any>, filter: Array<any>, filter_key: string): Array<any> {
-		if(filter.length == 0) return arr;
 		return arr.filter(arr_item => { 
 			for(let filter_item of filter) { 
 				if (arr_item[filter_key] == filter_item) return true; 
@@ -18,8 +17,17 @@ export class FilterSortService {
 	}
 
 	GetSearchedArray(arr: Array<any>, search_val: string, filter_key: string): Array<any> {
-		if(search_val.length == 0) return arr;
 		return arr.filter(arr_item => arr_item[filter_key].includes(search_val))
+	}
+
+	//根据首字母排序
+	GetSortedArray(arr: Array<any>, filter_key: string,insc:boolean): Array<any> {
+		if(insc) {
+			return arr.sort((item1,item2)=>item1.localeCompare(item2, 'zh-Hans-CN', {sensitivity: 'accent'}));
+		}
+		else {
+			return arr.sort((item1,item2)=>item1.localeCompare(item2, 'zh-Hans-CN', {sensitivity: 'accent'})).reverse();
+		}
 	}
 
 }
