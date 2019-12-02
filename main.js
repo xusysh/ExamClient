d776b93a66a4ad0c07d7f05822a4cc3acb8fbd43
gjy gjy <xusysh@126.com>
2019-12-02 13:10:39 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,42 +1,43 @@
-const {app, BrowserWindow} = require('electron')
-    const url = require("url");
-    const path = require("path");
+const { app, BrowserWindow } = require('electron')
+const url = require("url");
+const path = require("path");
 
-    let mainWindow
-    const Menu = electron.Menu
-    
-    function createWindow () {
-      // 隐藏菜单栏
-      Menu.setApplicationMenu(null)
-      mainWindow = new BrowserWindow({
-        width: 1920,
-        height: 1080,
-        webPreferences: {
-          nodeIntegration: true
-        }
-      })
+let mainWindow
 
-      mainWindow.loadURL(
-        url.format({
-          pathname: path.join(__dirname, `/dist/static/index.html`),
-          protocol: "file:",
-          slashes: true
-        })
-      );
-      // Open the DevTools.
-    //  mainWindow.webContents.openDevTools({mode:'detach'})
+function createWindow() {
 
-      mainWindow.on('closed', function () {
-        mainWindow = null
-      })
+  mainWindow = new BrowserWindow({
+    width: 1920,
+    height: 1080,
+    webPreferences: {
+      nodeIntegration: true
     }
+  })
 
-    app.on('ready', createWindow)
-
-    app.on('window-all-closed', function () {
-      if (process.platform !== 'darwin') app.quit()
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, `/dist/static/index.html`),
+      protocol: "file:",
+      slashes: true
     })
+  );
+  // Open the DevTools.
+  //  mainWindow.webContents.openDevTools({mode:'detach'})
 
-    app.on('activate', function () {
-      if (mainWindow === null) createWindow()
-    })
+  mainWindow.setFullScreen(true)
+  mainWindow.setMenu(null)
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow()
+})
