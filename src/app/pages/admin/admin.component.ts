27770a2592b3b47f54278af70aba542add39b95a
gjy gjy <xusysh@126.com>
2019-12-04 11:17:39 +0800

    update

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Route, ActivatedRoute,Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-admin',
@@ -11,7 +12,7 @@ export class AdminComponent implements OnInit {
   public user_name: string = null;
   public user_id:string = null;
 
-  constructor(private router:Router) { 
+  constructor(private router:Router,private http_client:HttpClient) { 
 
   }
 
@@ -28,6 +29,14 @@ export class AdminComponent implements OnInit {
   }
 
   Logout():void{
+    let base_url = sessionStorage.getItem("server_base_url");
+    let userid = sessionStorage.getItem("userid");
+    if (base_url == null || base_url == undefined || base_url == '') return;
+    if (userid == null || userid == undefined || userid == '') return;
+    let user_info = {
+      user_id: userid
+    };
+    this.http_client.post(base_url + 'upi/user/logout', user_info);
     sessionStorage.clear();
     this.router.navigateByUrl("/login");
   }
--- src/app/pages/student/student.component.ts
+++ src/app/pages/student/student.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Route, ActivatedRoute,Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-student',
@@ -11,7 +12,7 @@ export class StudentComponent implements OnInit {
   public user_id: string = null;
   public user_name:string = null;
 
-  constructor(private router:Router) { }
+  constructor(private router:Router,private http_client:HttpClient) { }
 
   ngOnInit() {
 
@@ -25,6 +26,14 @@ export class StudentComponent implements OnInit {
   }
 
   Logout():void{
+    let base_url = sessionStorage.getItem("server_base_url");
+    let userid = sessionStorage.getItem("userid");
+    if (base_url == null || base_url == undefined || base_url == '') return;
+    if (userid == null || userid == undefined || userid == '') return;
+    let user_info = {
+      user_id: userid
+    };
+    this.http_client.post(base_url + 'upi/user/logout', user_info);
     sessionStorage.clear();
     this.router.navigateByUrl("/login");
   }
