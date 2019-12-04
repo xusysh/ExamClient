8cbef62f8c569adc6c81fae25952a9f2455b65b5
gjy gjy <xusysh@126.com>
2019-12-04 11:15:32 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   isCollapsed = false;
+
+  constructor(private http_client: HttpClient) {
+  }
+
+  @HostListener('window:unload', ['$event'])
+  unloadHandler(event) {
+    // ...
+    //    console.log('unload');
+  }
+
+  @HostListener('window:beforeunload', ['$event'])
+  beforeUnloadHander(event) {
+    // ...
+    //    console.log('before unload');
+    let base_url = sessionStorage.getItem("server_base_url");
+    let userid = sessionStorage.getItem("userid");
+    if (base_url == null || base_url == undefined || base_url == '') return;
+    if (userid == null || userid == undefined || userid == '') return;
+    let user_info = {
+      user_id: userid
+    };
+    this.http_client.post(base_url + 'upi/user/logout', user_info);
+  }
+
 }
